02b22f13b5338af9c0f9bbd15ce5ee523648467e
Charles Monod-Broca <charles.monod-broca@neuf.fr>
2024-07-04 16:05:52 +0200

    Added support for both Zsh and Bash

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use clap_complete::{generate_to, shells::Zsh};
+use clap_complete::{generate_to, shells::Bash, shells::Zsh};
 use std::env;
 use std::io::Error;
 
@@ -6,17 +6,20 @@ include!("src/cli.rs");
 
 fn main() -> Result<(), Error> {
     let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
+        None => {
+            println!("cargo:warning=Will not generate any shell completion files. OUT_DIR environment variable is not set, which shouldn't happen in normal conditions.");
+            return Ok(());
+        }
         Some(outdir) => outdir,
     };
 
     let mut cmd = build_cli();
-    let path = generate_to(
-        Zsh,
-        &mut cmd,           // We need to specify what generator to use
-        "umivarcalextract", // We need to specify the bin name manually
-        outdir,             // We need to specify where to write to
-    )?;
+
+    let path = match env!("SHELL") {
+        "/usr/bin/zsh" => generate_to(Zsh, &mut cmd, "umivarcalextract", outdir)?,
+        "/usr/bin/bash" => generate_to(Bash, &mut cmd, "umivarcalextract", outdir)?,
+        _ => generate_to(Zsh, &mut cmd, "umivarcalextract", outdir)?,
+    };
 
     println!("cargo:warning=completion file is generated: {path:?}");
 
